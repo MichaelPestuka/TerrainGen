743ba99911391122da31f7316bb87f377856ae1d
MichaelPestuka <contact@michaelpestuka.cz>
2025-08-15 19:15:24 +0200

    feat: cliff customization sliders

--- TerrainGenFrontend/src/App.jsx
+++ TerrainGenFrontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react'
 import './App.css'
-import { Box, Button, Slider, Toolbar } from '@mui/material'
+import { Box, Button, Slider, TextField, Toolbar } from '@mui/material'
 import './Sliders.jsx'
 import InputSlider from './Sliders.jsx'
 import TerrainRenderer from './renderer.js'
@@ -9,17 +9,23 @@ import TerrainRenderer from './renderer.js'
 function App() {
   useEffect(() => {
       console.log("Starting Renderer");
-      renderer.current = new TerrainRenderer(worldSize, worldSize)
+      renderer.current = new TerrainRenderer({'Width':worldSize, 'Height':worldSize, 'Sealevel':seaLevel})
       // startRenderer();
   }, [])
 
+
   var renderer = useRef()
   const [worldSize, setSize] = useState(200)
+  const [seaLevel, setSeaLevel] = useState(0.5)
+  const [cliffMultiplier, setCliffMutliplier] = useState(6.0)
+  const [cliffOffset, setCliffOffset] = useState(0.0)
+  // const [seed, setSeed] = useState(Math.floor(Math.random() * 1000000))
 
-  const updateSize = (data) => {
-    setSize(data);
-  };
-
+  useEffect(() => {
+    // console.log(cliffMultiplier);
+    renderer.current.updateShaderValue("cliffMultiplier", cliffMultiplier);
+    renderer.current.updateShaderValue("cliffOffset", cliffOffset);
+  }, [cliffMultiplier, cliffOffset])
   return (
     <>
       <canvas id="c"></canvas>
@@ -28,9 +34,13 @@ function App() {
       <Box sx={{marginRight: '0', width: '20%', height: '100%', padding: '0 32px'}}>
         <Button variant='contained' onClick={() => {
           console.log("Reloafdinf")
-          renderer.current.fetchTerrain(worldSize, worldSize);
+          renderer.current.fetchTerrain({'Width':worldSize, 'Height':worldSize, 'Sealevel':seaLevel});
         }}> Regenerate </Button>
-        <InputSlider label={"World size"} updateData={updateSize} minValue={50} maxValue={300} defaultValue={200} />
+        <TextField label={"Seed"} type='number'/>
+        <InputSlider label={"World size"} updateData={setSize} minValue={50} maxValue={300} defaultValue={200} />
+        <InputSlider label={"Sea level"} updateData={setSeaLevel} minValue={0.0} maxValue={1.0} defaultValue={0.5} />
+        <InputSlider label={"Cliff Multiplier"} updateData={setCliffMutliplier} minValue={0.0} maxValue={10.0} defaultValue={6.0} />
+        <InputSlider label={"Cliff offset"} updateData={setCliffOffset} minValue={-0.1} maxValue={0.1} defaultValue={0.0} />
       </Box>
 
       </Box>
